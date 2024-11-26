f6da6efb38972ae4f55f56a4adad945fb77ae15d
jadhavy423 <108630265+jadhavy423@users.noreply.github.com>
2024-11-26 11:45:17 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -169,6 +169,8 @@
 import streamlit as st
 import pandas as pd
 import joblib  # Import joblib instead of pickle
+pip install xgboost
+import xgboost
 
 # Set custom CSS to change the background color to black
 st.markdown("""
